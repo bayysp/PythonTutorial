d94f434bd819966b960ab1f4508231d2e8192e81
Bayu Saputra <babahayoo@gmail.com>
2019-03-20 10:41:08 +0700

    SplitFunction added

---
+++ SplitFunction.py
@@ -0,0 +1,10 @@
+#split function is using for split a string with parameter
+#for example
+
+print("Bayu:Ai:Yola".split(":"))
+#it will put a 'Bayu' 'Ai' and 'Yola' into array
+
+#it also can be access by an index
+
+print("My Name is "+"Bayu:Ai:Yola".split(":")[1])
+#it will show 'My Name is Ai'
